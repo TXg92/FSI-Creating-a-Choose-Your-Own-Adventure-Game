9c677ddb367fd034caedf1d577d70e174d7623b9
Tsegyal <tsegyal.sakya@gmail.com>
2022-02-01 19:09:54 -0600

    firstanswer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let answer = window.prompt("Do you head left or right?");
+let firstAnswer = window.prompt("Do you head left or right?");
+// console.log("answer ", answer); // good for debugging!left
 if (firstAnswer === "left") {
   let secondAnswer = window.prompt(
     "You come across a stray cat.It scampers off down a small hole; just large enough for you to crawl through. Do you follow it, or continue on your path?"
